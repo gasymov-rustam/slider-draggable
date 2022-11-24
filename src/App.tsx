c8c3f8562d2462cc99b2f3564be384370b961741
Rustam Gasymov <rustam.gasymov@devalore.com>
2022-11-24 07:21:47 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactSlider from 'react-slider';
 
-function App() {
+const State = (props: { state: any; propss: any }) => {
+  const { state, propss } = props;
+  console.log(state, propss);
+  return <div {...propss}>{state.valueNow}</div>;
+};
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {/* <ReactSlider
+        className='horizontal-slider'
+        thumbClassName='example-thumb-custom'
+        trackClassName='example-track'
+        renderThumb={(propss, state) => <State propss={propss} state={state} />}
+      /> */}
+
+      <ReactSlider
+        className='horizontal-slider'
+        thumbClassName='example-thumb'
+        trackClassName='example-track'
+        defaultValue={[0, 100]}
+        ariaLabel={['Lower thumb', 'Upper thumb']}
+        ariaValuetext={(state) => `Thumb value ${state.valueNow}`}
+        renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
+        pearling
+        minDistance={10}
+        onChange={(value, index) => console.log(1111, value, index)}
+      />
+      <ReactSlider
+        className='horizontal-slider'
+        thumbClassName='example-thumb'
+        trackClassName='example-track'
+        defaultValue={[0, 50, 100]}
+        ariaLabel={['Leftmost thumb', 'Middle thumb', 'Rightmost thumb']}
+        renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
+        pearling={false}
+        minDistance={10}
+        step={5}
+        onChange={(value, index) => console.log(1111, value, index)}
+        snapDragDisabled
+      />
+      <ReactSlider
+        className='vertical-slider1'
+        thumbClassName='example-thumb1'
+        trackClassName='example-track'
+        marks={[10, 20, 40, 50, 70, 90]}
+        markClassName='example-track-custom2'
+        defaultValue={[0, 30, 70, 85, 100]}
+        orientation='vertical'
+        ariaLabel={['Leftmost thumb', 'Rightmost thumb']}
+        renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
+        step={5}
+        pearling
+        minDistance={10}
+      />
+      <ReactSlider
+        className='vertical-slider'
+        thumbClassName='example-thumb'
+        trackClassName='example-track'
+        defaultValue={[0, 50, 100]}
+        ariaLabel={['Lowest thumb', 'Middle thumb', 'Top thumb']}
+        renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
+        orientation='vertical'
+        invert
+        pearling
+        minDistance={10}
+      />
+      <ReactSlider
+        className='horizontal-slider'
+        marks={[5, 10, 35, 40, 95]}
+        markClassName='example-mark'
+        // min={0}
+        // max={5}
+        thumbClassName='example-thumb'
+        trackClassName='example-track'
+        renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
+      />
+      <ReactSlider
+        className='horizontal-slider'
+        thumbClassName='example-thumb'
+        trackClassName='example-track'
+        marks={[10, 20, 40, 50, 70, 90]}
+        markClassName='example-track-custom'
+        defaultValue={[0, 30, 70, 85, 100]}
+        ariaLabel={['Leftmost thumb', 'Rightmost thumb']}
+        renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
+        pearling
+        minDistance={10}
+      />
     </div>
   );
-}
-
-export default App;
+};
